e80082af58fff950ac1d246d94a61b5e28e347b2
Stanley <mstanley001@e.ntu.edu.sg>
2020-01-28 19:58:31 +0800

    added workshop

---
+++ workshops.js
@@ -0,0 +1,12 @@
+let title1 = "Arduino";
+let title2 = "ESP32";
+let title3 = "Sex";
+var text1 = "21 Jan";
+var text2 = "22 Jan";
+var text3 = "23 Jan";
+let content1 = document.getElementById("content-text-1");
+content1.innerHTML = 'titit';
+document.getElementById("content-block-2").getElementsByClassName("image-content").getElementsByTagName("p").innerHTML = 'titit';
+document.getElementById("content-block-3").getElementsByClassName("image-content").getElementsByTagName("p").innerHTML = title3;
+
+
