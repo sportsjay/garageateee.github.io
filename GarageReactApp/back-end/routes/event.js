5e1d61a3a64e1474515fabf45b84f91c263819e1
Francisco Bryant <frbryant00@gmail.com>
2020-06-04 02:14:10 +0800

    implement svg wave to events and workshop div

--- GarageReactApp/back-end/routes/event.js
+++ GarageReactApp/back-end/routes/event.js
@@ -1,32 +1,34 @@
-const router = require('express').Router();
-let Event = require('../models/event.model');
+const router = require("express").Router();
+let Event = require("../models/event.model");
 
-router.route('/').get((req, respo) => {
-    Event.find()
-        .then(event => respo.json(event))
-        .catch(err => respo.status(400).json('Error: '+ err));
+router.route("/").get((req, respo) => {
+	Event.find()
+		.then((event) => respo.json(event))
+		.catch((err) => respo.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req, respo) => {
-    const id            = req.body.id;
-    const name          = req.body.name;
-    // const date       = Date.parse(req.body.date);
-    const location      = req.body.location;
-    const description   = req.body.description;
+router.route("/add").post((req, respo) => {
+	const id = req.body.id;
+	const name = req.body.name;
+	// const date       = Date.parse(req.body.date);
+	const dateStart = req.body.dateStart;
+	const dateEnd = req.body.dateEnd;
+	const location = req.body.location;
+	const description = req.body.description;
 
-    const newEvent  = new Event({
-        id,
-        name,
-        // date,
-        location,
-        description,
-    });
+	const newEvent = new Event({
+		id,
+		name,
+		dateStart,
+		dateEnd,
+		location,
+		description,
+	});
 
-    newEvent.save()
-    .then(() => respo.json('Exercise added!'))
-    .catch(err => respo.status(400).json('Error: '+ err));
-})
+	newEvent
+		.save()
+		.then(() => respo.json("Exercise added!"))
+		.catch((err) => respo.status(400).json("Error: " + err));
+});
 
 module.exports = router;
-
-    
--- GarageReactApp/back-end/routes/workshop.js
+++ GarageReactApp/back-end/routes/workshop.js
@@ -1,30 +1,34 @@
-const router = require('express').Router();
-let Workshop = require('../models/workshop.model');
+const router = require("express").Router();
+let Workshop = require("../models/workshop.model");
 
-router.route('/').get((req, respo) => {
-    Workshop.find()
-        .then(workshop => respo.json(workshop))
-        .catch(err => respo.status(400).json('Error: '+ err));
+router.route("/").get((req, respo) => {
+	Workshop.find()
+		.then((workshop) => respo.json(workshop))
+		.catch((err) => respo.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req, respo) => {
-    const id = req.body.id;
-    const name = req.body.name;
-    // const date = Date.parse(req.body.date);
-    const location = req.body.location;
-    const description = req.body.description;
+router.route("/add").post((req, respo) => {
+	const id = req.body.id;
+	const name = req.body.name;
+	// const date = Date.parse(req.body.date);
+	const dateStart = req.body.dateStart;
+	const dateEnd = req.body.dateEnd;
+	const location = req.body.location;
+	const description = req.body.description;
 
-    const newWorkshop  = new Workshop({
-        id,
-        name,
-        // date,
-        location,
-        description,
-    });
+	const newWorkshop = new Workshop({
+		id,
+		name,
+		dateStart,
+		dateEnd,
+		location,
+		description,
+	});
 
-    newWorkshop.save()
-        .then(() => respo.json('Workshop added!'))
-        .catch(err => respo.status(400).json('Error: '+ err));
-})
+	newWorkshop
+		.save()
+		.then(() => respo.json("Workshop added!"))
+		.catch((err) => respo.status(400).json("Error: " + err));
+});
 
 module.exports = router;
